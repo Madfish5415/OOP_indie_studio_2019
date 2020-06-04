28d051b681865eb46ba2f844d9688a881fa6f944
Matteo Fauchon <matteo.fauchon@epitech.eu>
2020-06-04 19:53:00 +0200

    fix:  changed header file's name

--- src/irrlicht/vector3df.cpp
+++ src/irrlicht/vector3df.cpp
@@ -2,12 +2,13 @@
 ** EPITECH PROJECT, 2020
 ** IndieStudio
 ** File description:
-** irrlicht.cpp
+** vector3df.cpp
 */
 
 #include <irrlicht.h>
 
-irr::core::vector3df operator*(const irr::core::vector3df& vec, irr::f32 value) {
+irr::core::vector3df operator*(const irr::core::vector3df& vec, irr::f32 value)
+{
     irr::core::vector3df tmp;
     tmp.X = vec.X * value;
     tmp.Y = vec.Y * value;
