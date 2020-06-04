549fc3f230f0e34f45ab24890a2050788828a2b0
lucas <lucas.fabreguettes@epitech.eu>
2020-06-04 15:14:21 +0200

    feat: ecs Core

---
+++ src/Core/Core.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** Core.hpp
+*/
+
+#ifndef OOP_INDIE_STUDIO_2019_CORE_HPP
+#define OOP_INDIE_STUDIO_2019_CORE_HPP
+
+#include "../ecs/Universe.hpp"
+
+namespace core {
+    class Core {
+      public:
+        Core();
+        ~Core();
+
+        void init();
+        [[noreturn]] void run();
+
+      private:
+        std::unique_ptr<ecs::Universe> _universe;
+    };
+}
+
+#endif // OOP_INDIE_STUDIO_2019_CORE_HPP
