f65536afb21bf11ce110628d99360b1f80dfdad1
Lucas Arsene <lucas.arsene@epitech.eu>
2020-06-04 16:42:17 +0200

    feat: adding Render system

---
+++ src/ecs/system/Render.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** Render
+*/
+
+#include "Render.hpp"
+#include "../Universe.hpp"
+
+
+using namespace ecs::system;
+
+Render::Render(WorldManager *worldManager) : ecs::System(worldManager)
+{
+    irr::IrrlichtDevice *device = worldManager->getUniverse()->getDevice();
+
+    smgr = device->getSceneManager();
+    gui = device->getGUIEnvironment();
+    driver = device->getVideoDriver();
+}
+
+Render::~Render() = default;
+
+void Render::update() 
+{
+    if (driver) {
+        driver->beginScene();
+        if (gui)
+            gui->drawAll();
+        if (smgr)
+            smgr->drawAll();
+        driver->endScene();
+    }
+}
+
+
---
+++ src/ecs/system/Render.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** Render.hpp
+*/
+
+#ifndef OOP_INDIE_STUDIO_2019_RENDER_HPP
+#define OOP_INDIE_STUDIO_2019_RENDER_HPP
+
+#include <irrlicht.h>
+
+#include "../System.hpp"
+#include "../WorldManager.hpp"
+
+namespace ecs::system {
+
+class Render : public ecs::System {
+    public:
+        explicit Render(WorldManager *worldManager);
+        ~Render();
+
+    public:
+        void update();
+
+    public:
+        irr::scene::ISceneManager *smgr;
+        irr::gui::IGUIEnvironment *gui;
+        irr::video::IVideoDriver *driver;
+};
+}
+
+#endif /* !RENDER_HPP_ */
