a68fa5ce2773070263fae2e1a9863623e0a142e3
Matteo Fauchon <matteo.fauchon@epitech.eu>
2020-06-04 23:08:16 +0200

    feat: add background, logo
    fix: the buttons' disposition

--- src/scene/Menu.cpp
+++ src/scene/Menu.cpp
@@ -9,6 +9,8 @@
 
 #include "../ecs/Universe.hpp"
 #include "../ecs/component/Button.hpp"
+#include "../ecs/component/Image.hpp"
+#include "../ecs/system/Image.hpp"
 #include "../ecs/system/Render.hpp"
 
 using namespace scene;
@@ -18,19 +20,37 @@ static void createButton(ecs::WorldManager* worldManager, irr::gui::IGUIEnvironm
     const wchar_t* tooltipText = nullptr)
 {
     ecs::Entity button = worldManager->createEntity();
-    worldManager->addComponent<ecs::component::Button>(button, ecs::component::Button(gui, rect, parent, id, text, tooltipText));
+    worldManager->addComponent<ecs::component::Button>(
+        button, ecs::component::Button(gui, rect, parent, id, text, tooltipText));
 }
 
 void Menu::init(ecs::WorldManager* worldManager)
 {
     auto gui = worldManager->getUniverse()->getDevice()->getGUIEnvironment();
+    auto driver = worldManager->getUniverse()->getDevice()->getVideoDriver();
 
     worldManager->registerComponent<ecs::component::Button>();
+    worldManager->registerComponent<ecs::component::Image>();
     worldManager->registerSystem<ecs::system::Render>();
-    createButton(
-        worldManager, gui, new irr::core::rect<irr::s32>(960 - 200, 300, 960 + 200, 300 + 60), nullptr, GUI_MENU_PLAY, L"PLAY");
-    createButton(
-        worldManager, gui, new irr::core::rect<irr::s32>(960 - 200, 400, 960 + 200, 400 + 60), nullptr, GUI_MENU_QUIT, L"QUIT");
-    createButton(worldManager, gui, new irr::core::rect<irr::s32>(960 - 200, 500, 960 + 200, 500 + 60), nullptr, GUI_MENU_HTP,
-        L"HOW TO PLAY");
+    worldManager->registerSystem<ecs::system::Image>();
+    {
+        ecs::Signature signature;
+
+        signature.set(worldManager->getComponentType<ecs::component::Image>());
+        worldManager->setSystemSignature<ecs::system::Image>(signature);
+    }
+    ecs::Entity background = worldManager->createEntity();
+    worldManager->addComponent(background,
+        ecs::component::Image(driver, "./assets/background.jpg", new irr::core::position2d<irr::s32> {0, 0},
+            new irr::core::rect<irr::s32> {0, 0, 1920, 1080}));
+    ecs::Entity bombermanLogo = worldManager->createEntity();
+    worldManager->addComponent(bombermanLogo,
+        ecs::component::Image(driver, "./assets/bomberman_logo.png", new irr::core::position2d<irr::s32> {960 - 364, 100},
+            new irr::core::rect<irr::s32> {0, 0, 728, 202}));
+    createButton(worldManager, gui, new irr::core::rect<irr::s32>(400 - 200, 800, 400 + 200, 800 + 60), nullptr,
+        GUI_MENU_PLAY, L"PLAY");
+    createButton(worldManager, gui, new irr::core::rect<irr::s32>(960 - 200, 800, 960 + 200, 800 + 60), nullptr,
+        GUI_MENU_QUIT, L"QUIT");
+    createButton(worldManager, gui, new irr::core::rect<irr::s32>(1520 - 200, 800, 1520 + 200, 800 + 60), nullptr,
+        GUI_MENU_HTP, L"HOW TO PLAY");
 }
