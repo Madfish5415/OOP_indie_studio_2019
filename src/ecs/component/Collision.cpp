5c2739e98bf13d6da5930484feda37c11efa36c3
Lucas Arsene <lucas.arsene@epitech.eu>
2020-06-07 18:51:07 +0200

    feat: adding collision for players and bots

---
+++ src/ecs/component/Collision.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** Collision
+*/
+
+#include "Collision.hpp"
+
+using namespace ecs::component;
+
+Collision::Collision()
+{
+}
+
+Collision::~Collision()
+{
+}
