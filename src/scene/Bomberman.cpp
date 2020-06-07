be1d0db22d992337a48f10d6f620bdf288b34221
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-06-08 00:36:46 +0200

    refactor: Fix characters typo

--- src/scene/Bomberman.cpp
+++ src/scene/Bomberman.cpp
@@ -30,21 +30,21 @@ std::vector<ecs::Entity> Bomberman::playerIds = {};
 irr::scene::IMetaTriangleSelector *Bomberman::metaTriangleSelector = nullptr;
 
 static irr::scene::IAnimatedMeshSceneNode *addCollisions(
-    ecs::WorldManager *worldManager, irr::scene::ISceneManager *smgr, irr::scene::IAnimatedMeshSceneNode *caracterMesh)
+    ecs::WorldManager *worldManager, irr::scene::ISceneManager *smgr, irr::scene::IAnimatedMeshSceneNode *characterMesh)
 {
     irr::scene::ITriangleSelector *selector =
-        smgr->createOctreeTriangleSelector(caracterMesh->getMesh(), caracterMesh, 128);
-    caracterMesh->setTriangleSelector(selector);
+        smgr->createOctreeTriangleSelector(characterMesh->getMesh(), characterMesh, 128);
+    characterMesh->setTriangleSelector(selector);
 
     irr::core::aabbox3d<irr::f32> box(0, 0, 0, 9.5, 9.5, 9.5);
     irr::core::vector3df radius = box.MaxEdge - box.getCenter();
 
     irr::scene::ISceneNodeAnimator *anim = smgr->createCollisionResponseAnimator(Bomberman::metaTriangleSelector,
-        caracterMesh, radius, irr::core::vector3df(0, -10, 0), irr::core::vector3df(0, 0, 0));
-    caracterMesh->addAnimator(anim);
+        characterMesh, radius, irr::core::vector3df(0, -10, 0), irr::core::vector3df(0, 0, 0));
+    characterMesh->addAnimator(anim);
     anim->drop();
     selector->drop();
-    return (caracterMesh);
+    return (characterMesh);
 }
 
 static void createPlayer(ecs::WorldManager *worldManager, const ecs::component::Player &player_comp,
@@ -52,36 +52,36 @@ static void createPlayer(ecs::WorldManager *worldManager, const ecs::component::
 {
     irr::scene::ISceneManager *smgr = worldManager->getUniverse()->getDevice()->getSceneManager();
     irr::video::IVideoDriver *driver = worldManager->getUniverse()->getDevice()->getVideoDriver();
-    ecs::Entity caracter = worldManager->createEntity();
-    irr::scene::IAnimatedMeshSceneNode *caracterMesh =
+    ecs::Entity character = worldManager->createEntity();
+    irr::scene::IAnimatedMeshSceneNode *characterMesh =
         smgr->addAnimatedMeshSceneNode(smgr->getMesh(bomberman::ninja::NINJA.c_str()));
 
-    caracterMesh->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-    caracterMesh->setPosition(pos);
+    characterMesh->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+    characterMesh->setPosition(pos);
     if (charNbr == 0 || charNbr == 1)
-        caracterMesh->setRotation(irr::core::vector3df(0, 90, 0));
+        characterMesh->setRotation(irr::core::vector3df(0, 90, 0));
     else
-        caracterMesh->setRotation(irr::core::vector3df(0, -90, 0));
-    caracterMesh->setScale(irr::core::vector3df(1.9, 1.9, 1.9));
-    caracterMesh->setFrameLoop(183, 204);
+        characterMesh->setRotation(irr::core::vector3df(0, -90, 0));
+    characterMesh->setScale(irr::core::vector3df(1.9, 1.9, 1.9));
+    characterMesh->setFrameLoop(183, 204);
 
-    caracterMesh->setMaterialTexture(0, driver->getTexture(path.c_str()));
+    characterMesh->setMaterialTexture(0, driver->getTexture(path.c_str()));
     bomberman::ninja::PLAYER_SKINS[path] = true;
 
-    caracterMesh = addCollisions(worldManager, smgr, caracterMesh);
+    characterMesh = addCollisions(worldManager, smgr, characterMesh);
 
-    worldManager->addComponent<ecs::component::Render3d>(caracter, ecs::component::Render3d(caracterMesh));
-    worldManager->addComponent<ecs::component::Player>(caracter, player_comp);
-    worldManager->addComponent<ecs::component::Motion>(caracter, ecs::component::Motion());
+    worldManager->addComponent<ecs::component::Render3d>(character, ecs::component::Render3d(characterMesh));
+    worldManager->addComponent<ecs::component::Player>(character, player_comp);
+    worldManager->addComponent<ecs::component::Motion>(character, ecs::component::Motion());
     worldManager->addComponent<ecs::component::Transform>(
-        caracter, ecs::component::Transform(caracterMesh->getPosition()));
-    worldManager->addComponent<ecs::component::Stats>(caracter, ecs::component::Stats());
-    worldManager->addComponent<ecs::component::Animation>(caracter,
+        character, ecs::component::Transform(characterMesh->getPosition()));
+    worldManager->addComponent<ecs::component::Stats>(character, ecs::component::Stats());
+    worldManager->addComponent<ecs::component::Animation>(character,
         ecs::component::Animation(
             std::unordered_map<std::string, std::pair<size_t, size_t>>({{"IDLE", {183, 204}}, {"WALKING", {0, 13}}})));
-    worldManager->addComponent<ecs::component::Collision>(caracter, ecs::component::Collision());
+    worldManager->addComponent<ecs::component::Collision>(character, ecs::component::Collision());
 
-    Bomberman::playerIds.push_back(caracter);
+    Bomberman::playerIds.push_back(character);
 }
 
 static const std::string getUnusedSkin()
@@ -103,40 +103,40 @@ static void createBot(ecs::WorldManager *worldManager, irr::core::vector3df pos,
 {
     irr::scene::ISceneManager *smgr = worldManager->getUniverse()->getDevice()->getSceneManager();
     irr::video::IVideoDriver *driver = worldManager->getUniverse()->getDevice()->getVideoDriver();
-    ecs::Entity caracter = worldManager->createEntity();
-    irr::scene::IAnimatedMeshSceneNode *caracterMesh =
+    ecs::Entity character = worldManager->createEntity();
+    irr::scene::IAnimatedMeshSceneNode *characterMesh =
         smgr->addAnimatedMeshSceneNode(smgr->getMesh(bomberman::ninja::NINJA.c_str()));
 
-    caracterMesh->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-    caracterMesh->setPosition(pos);
+    characterMesh->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+    characterMesh->setPosition(pos);
     if (charNbr == 0 || charNbr == 1)
-        caracterMesh->setRotation(irr::core::vector3df(0, 90, 0));
+        characterMesh->setRotation(irr::core::vector3df(0, 90, 0));
     else
-        caracterMesh->setRotation(irr::core::vector3df(0, -90, 0));
-    caracterMesh->setScale(irr::core::vector3df(1.9, 1.9, 1.9));
-    caracterMesh->setFrameLoop(183, 204);
+        characterMesh->setRotation(irr::core::vector3df(0, -90, 0));
+    characterMesh->setScale(irr::core::vector3df(1.9, 1.9, 1.9));
+    characterMesh->setFrameLoop(183, 204);
 
-    caracterMesh->setMaterialTexture(0, driver->getTexture(getUnusedSkin().c_str()));
+    characterMesh->setMaterialTexture(0, driver->getTexture(getUnusedSkin().c_str()));
 
-    // caracterMesh = addCollisions(worldManager, smgr, caracterMesh);
+    // characterMesh = addCollisions(worldManager, smgr, characterMesh);
 
-    worldManager->addComponent<ecs::component::Render3d>(caracter, ecs::component::Render3d(caracterMesh));
-    worldManager->addComponent<ecs::component::AI>(caracter, ecs::component::AI());
-    worldManager->addComponent<ecs::component::Motion>(caracter, ecs::component::Motion());
+    worldManager->addComponent<ecs::component::Render3d>(character, ecs::component::Render3d(characterMesh));
+    worldManager->addComponent<ecs::component::AI>(character, ecs::component::AI());
+    worldManager->addComponent<ecs::component::Motion>(character, ecs::component::Motion());
     worldManager->addComponent<ecs::component::Transform>(
-        caracter, ecs::component::Transform(caracterMesh->getPosition()));
-    worldManager->addComponent<ecs::component::Stats>(caracter, ecs::component::Stats());
-    worldManager->addComponent<ecs::component::Animation>(caracter,
+        character, ecs::component::Transform(characterMesh->getPosition()));
+    worldManager->addComponent<ecs::component::Stats>(character, ecs::component::Stats());
+    worldManager->addComponent<ecs::component::Animation>(character,
         ecs::component::Animation(
             std::unordered_map<std::string, std::pair<size_t, size_t>>({{"IDLE", {183, 204}}, {"WALKING", {0, 13}}})));
-    worldManager->addComponent<ecs::component::Collision>(caracter, ecs::component::Collision());
+    worldManager->addComponent<ecs::component::Collision>(character, ecs::component::Collision());
 }
 
-static void createCaracters(ecs::WorldManager *worldManager, irr::u32 tileSize, irr::u32 nbTile,
+static void createCharacters(ecs::WorldManager *worldManager, irr::u32 tileSize, irr::u32 nbTile,
     std::vector<ecs::component::Player> players, std::vector<std::string> paths)
 {
     irr::f32 offset = tileSize / 2;
-    std::vector<irr::core::vector3df> caracterPositions = {
+    std::vector<irr::core::vector3df> characterPositions = {
         irr::core::vector3df(tileSize + offset, 5.0, tileSize + offset),
         irr::core::vector3df(tileSize + offset, 5.0, tileSize * (nbTile - 2) + offset),
         irr::core::vector3df(tileSize * (nbTile - 2) + offset, 5.0, tileSize + offset),
@@ -144,9 +144,9 @@ static void createCaracters(ecs::WorldManager *worldManager, irr::u32 tileSize, 
 
     for (size_t i = 0; i < 4; i++) {
         if (i < players.size()) {
-            createPlayer(worldManager, players[i], caracterPositions[i], i, paths[i]);
+            createPlayer(worldManager, players[i], characterPositions[i], i, paths[i]);
         } else {
-            createBot(worldManager, caracterPositions[i], i);
+            createBot(worldManager, characterPositions[i], i);
         }
     }
 }
@@ -310,7 +310,7 @@ void scene::Bomberman::init(
     worldManager->addComponent<ecs::component::Render3d>(camera, ecs::component::Render3d(cameraNode));
 
     createMap(worldManager, tileSize);
-    createCaracters(worldManager, tileSize, nbTile, players, paths);
+    createCharacters(worldManager, tileSize, nbTile, players, paths);
 }
 
 void scene::Bomberman::destroy(ecs::Universe *universe)
