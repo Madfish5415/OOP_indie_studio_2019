06d41ca42315866d07223cb95cc64ab32094163e
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-06-10 23:44:02 +0200

    fix: Fix sound effects

--- src/ecs/system/Sound.cpp
+++ src/ecs/system/Sound.cpp
@@ -22,6 +22,7 @@ void Sound::update()
     for (const auto& entity: entities) {
         auto& sounds = worldManager->getComponent<ecs::component::Sound>(entity);
         for (auto& sound : sounds.soundsToPlay) {
+            sounds.sounds[sound]->setPlayingOffset(sf::Time::Zero);
             sounds.sounds[sound]->play();
             sounds.soundsToPlay.erase(std::find(sounds.soundsToPlay.begin(), sounds.soundsToPlay.end(), sound));
         }
